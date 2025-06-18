4d492e088708da98623c2b76e174ea7d5a384f06
Pichyapa Kh <dduidokdoi@gmail.com>
2025-06-18 19:51:26 +0700

    fix: pipeline

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,6 +2,7 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
     testDir: './src/e2e',
+    testMatch: /.*\.spec\.ts/,
     timeout: 30 * 1000,
     retries: 0,
     use: {

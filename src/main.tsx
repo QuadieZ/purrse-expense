317aefce68d16cb7c486aa55ae390c08935c094b
Pichyapa Kh <dduidokdoi@gmail.com>
2025-06-19 18:41:24 +0700

    feat: performance tracker

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import { App, MeowKitProvider } from '@/app';
 import { Toaster } from '@/common/ui';
 import '@fontsource-variable/quicksand/index.css';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import { createRoot } from 'react-dom/client';
 
 createRoot(document.getElementById('root')!).render(
   <MeowKitProvider>
     <Toaster />
     <App />
+    <SpeedInsights />
   </MeowKitProvider>,
 );
